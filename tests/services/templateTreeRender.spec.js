56053123d27098e090afb195cc92775485a4001c
Artem Demo <artemdemo@gmail.com>
2016-05-21 16:35:03 +0300

    added mocha and jsdom

---
+++ tests/services/templateTreeRender.spec.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import {templateTreeRender} from '../../source/services/templateTreeRender';
+
+describe('templateTreeRender', () => {
+    it('test', () => {
+        
+    });
+});
