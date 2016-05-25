bce36dfde128f0b424b33a3a278f930562a48ff2
Artem Demo <artemdemo@gmail.com>
2016-05-25 07:54:47 +0300

    Remove tmp 'is-typing' from service. Example of using 'is-typing'

--- source/services/dialogList.js
+++ source/services/dialogList.js
@@ -5,12 +5,28 @@ export const dialogList = (() => {
 
     return {
         addPhrase: (newPhrase) => {
-            dialog.push({
-                side: newPhrase.side,           // 'chat', 'user'
-                message: newPhrase.message,     // text of the message
-                ref: newPhrase.ref || null,     // bubble element reference
-                type: newPhrase.type            // 'is-typing'
-            });
+            let temporaryPhrase;
+            let temporaryPhraseIndex;
+            for (let i = 0, len = dialog.length; i < len; i++) {
+                if (dialog[i].type === IS_TYPING) {
+                    temporaryPhrase = dialog[i];
+                    temporaryPhraseIndex = i;
+                }
+            }
+            if (temporaryPhrase && newPhrase.side !== temporaryPhrase.side) {
+                // insert before
+                dialog.splice(temporaryPhraseIndex, 0, newPhrase);
+            } else {
+                if (temporaryPhrase) {
+                    dialog.splice(temporaryPhraseIndex, 1);
+                }
+                dialog.push({
+                    side: newPhrase.side,           // 'chat', 'user'
+                    message: newPhrase.message,     // text of the message
+                    ref: newPhrase.ref || null,     // bubble element reference
+                    type: newPhrase.type            // 'is-typing'
+                });
+            }
         },
 
         getTemporaryPhrase: () => {
